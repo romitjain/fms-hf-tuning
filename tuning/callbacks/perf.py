53bf27803f061e2dd21ef12ac51a95bf47774452
romit <romit@ibm.com>
2025-12-17 11:54:26 +0000

    Added sys metrics

--- tuning/callbacks/perf.py
+++ tuning/callbacks/perf.py
@@ -14,49 +14,26 @@ class PerfCallback(TrainerCallback):
             torch.cuda.reset_peak_memory_stats()
         self._t0 = time.perf_counter()
 
-    def on_step_end(self, args, state, control, **kwargs):
-        if state.global_step == 0 or state.global_step % self.log_every_steps != 0:
-            return
-
+    def on_log(self, args, state, control, logs=None, **kwargs):
         now = time.perf_counter()
         dt = now - (self._t0 or now)
+        self._t0 = now
 
-        mem_alloc_gib = None
-        mem_resv_gib = None
-        if torch.cuda.is_available():
+        if self.accelerator is not None and torch.cuda.is_available():
             mem_alloc_gib = torch.cuda.max_memory_allocated() / (1024**3)
             mem_resv_gib = torch.cuda.max_memory_reserved() / (1024**3)
 
-
-        if self.accelerator is not None and torch.cuda.is_available():
             a = torch.tensor([mem_alloc_gib], device=self.accelerator.device, dtype=torch.float64)
             r = torch.tensor([mem_resv_gib], device=self.accelerator.device, dtype=torch.float64)
             mem_alloc_gib = float(self.accelerator.gather(a).max().item())
             mem_resv_gib = float(self.accelerator.gather(r).max().item())
 
-        self.perf_metrics  = {f"time_per_{self.log_every_steps}_steps": dt}
-
-        if mem_alloc_gib is not None:
-            self.perf_metrics.update({
-                "peak_mem_alloc_gib": mem_alloc_gib,
-                "peak_mem_reserved_gib": mem_resv_gib
-            }) # type: ignore
-
-        self._t0 = now
-        if torch.cuda.is_available():
             torch.cuda.reset_peak_memory_stats()
 
-    def on_log(self, args, state, control, logs=None, **kwargs):
-        if logs is None:
-            return
-
-        if hasattr(self, 'perf_metrics') and (self.accelerator is None or self.accelerator.is_main_process):
-            logs.update(self.perf_metrics)
-            self.perf_metrics = {}
-
-            if state.log_history:
-                state.log_history[-1].update(self.perf_metrics)
-
-        # if self.accelerator.is_main_process:
-        #     import pdb
-        #     pdb.set_trace()
+        if logs:
+            sys_metrics  = {
+                "time_per_steps": dt,
+                "peak_mem_alloc_gib": mem_alloc_gib if mem_alloc_gib is not None else -1,
+                "peak_mem_reserved_gib": mem_resv_gib if mem_resv_gib is not None else -1
+            }
+            logs.update(**sys_metrics)
--- tuning/sft_trainer.py
+++ tuning/sft_trainer.py
@@ -568,7 +568,7 @@ def train(
         tc_callback.on_init_end(trainer.args, trainer.state, trainer.control)
         trainer.add_callback(tc_callback)
 
-    trainer.add_callback(PerfCallback(accelerator=trainer.accelerator, log_every_steps=1))
+    # trainer.add_callback(PerfCallback(accelerator=trainer.accelerator, log_every_steps=1))
     trainer.train(resume_from_checkpoint)
     additional_metadata = {}
     additional_metadata["added_tokens_info"] = added_tokens_dict
