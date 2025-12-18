c1a0345d7a09c480f9c43896cfab66aecf31ec82
romit <romit@ibm.com>
2025-12-18 04:37:45 +0000

    fp8 config

--- config/fp8.sh
+++ config/fp8.sh
@@ -1,2 +1,2 @@
-SFT_TRAINER_CONFIG_JSON_PATH=./config/sft.json \
+SFT_TRAINER_CONFIG_JSON_PATH=./config/sft_fp8.json \
 accelerate launch --config_file ./config/fsdp_mp_fp8.yaml -m tuning.sft_trainer
